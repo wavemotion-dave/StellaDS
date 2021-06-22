fa51e275e90b107f4a13f30fd6db7d422138d04a
wavemotion-dave <75039837+wavemotion-dave@users.noreply.github.com>
2021-06-22 09:08:34 -0400

    Another 10 instruction manuals added.

--- arm9/source/emucore/M6502Low.ITCM.cpp
+++ arm9/source/emucore/M6502Low.ITCM.cpp
@@ -42,7 +42,7 @@ M6502Low::M6502Low(uInt32 systemCyclesPerProcessorCycle)
     : M6502(systemCyclesPerProcessorCycle)
 {
     NumberOfDistinctAccesses = 0;
-    asm(".rept 7 ; nop ; .endr");
+    asm(".rept 5 ; nop ; .endr");
 }
 
 // - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
