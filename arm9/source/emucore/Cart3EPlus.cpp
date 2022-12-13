18e31d1af41b97a243a75cc2b45725047202c5b8
wavemotion-dave <75039837+wavemotion-dave@users.noreply.github.com>
2022-12-13 07:25:39 -0500

    Adding 3E+ Banking driver

---
+++ arm9/source/emucore/Cart3EPlus.cpp
@@ -0,0 +1,199 @@
+//============================================================================
+//
+//   SSSS    tt          lll  lll
+//  SS  SS   tt           ll   ll
+//  SS     tttttt  eeee   ll   ll   aaaa
+//   SSSS    tt   ee  ee  ll   ll      aa
+//      SS   tt   eeeeee  ll   ll   aaaaa  --  "An Atari 2600 VCS Emulator"
+//  SS  SS   tt   ee      ll   ll  aa  aa
+//   SSSS     ttt  eeeee llll llll  aaaaa
+//
+// Copyright (c) 1995-2022 by Bradford W. Mott, Stephen Anthony
+// and the Stella Team
+//
+// This file has been modified by Dave Bernazzani (wavemotion-dave)
+// for optimized execution on the DS/DSi platform. Please seek the
+// official Stella source distribution which is far cleaner, newer,
+// and better maintained.
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//============================================================================
+#include <assert.h>
+#include "Random.hxx"
+#include "Cart3EPlus.hxx"
+#include "System.hxx"
+#include "TIA.hxx"
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Cartridge3EPlus::Cartridge3EPlus(const uInt8* image, uInt32 size)
+  : mySize(size)
+{
+  // Copy the ROM image into my buffer - just reuse the existing cart_buffer[] to save memory
+  myImage = (uInt8 *)image;
+
+  // Initialize RAM with random values
+  class Random random;
+  for(uInt32 i = 0; i < 32768; ++i)
+  {
+    my3ERam[i] = random.next();
+  }
+  
+  // ----------------------------------------------------------------------
+  // According to online docs, for 3E+ the only thing that really matters 
+  // is that the first (0th) bank of the image is slotted into segment 3
+  // but we will initalize all segments 0-3 with this starting bank...
+  // ----------------------------------------------------------------------
+  statingBank = 0;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+Cartridge3EPlus::~Cartridge3EPlus()
+{
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+const char* Cartridge3EPlus::name() const
+{
+  return "Cartridge3EPlus";
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Cartridge3EPlus::reset()
+{
+  // Restore the original starting bank to all segments...
+  segment(0, statingBank);
+  segment(1, statingBank);
+  segment(2, statingBank);
+  segment(3, statingBank);
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Cartridge3EPlus::install(System& system)
+{
+  mySystem = &system;
+  uInt16 shift = mySystem->pageShift();
+
+  // Set the page accessing methods for the hot spots (for 100% emulation
+  // we need to chain any accesses below 0x40 to the TIA. Our poke() method
+  // does this via mySystem->tiaPoke(...) to chain the access.
+  for(uInt32 i = 0x00; i < 0x80; i += (1 << shift))
+  {
+    page_access.directPeekBase = 0;
+    page_access.directPokeBase = 0;
+    page_access.device = this;
+    mySystem->setPageAccess(i >> shift, page_access);
+  }
+
+  // Start with nothing mapped for ROM access - we'll install segments below
+  for(uInt32 j = 0x1000; j < 0x2000; j += (1 << shift))
+  {
+    page_access.device = this;
+    page_access.directPeekBase = 0;
+    page_access.directPokeBase = 0;
+    mySystem->setPageAccess(j >> shift, page_access);
+  }
+    
+  // Install pages for all segments of the ROM
+  segment(0, statingBank);
+  segment(1, statingBank);
+  segment(2, statingBank);
+  segment(3, statingBank);
+}
+
+extern TIA* theTIA; // 3E+ games utilize hotspot bytes in the lower 80h so we need to chain to the real peek addresses...
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+uInt8 Cartridge3EPlus::peek(uInt16 address)
+{
+  if (address < 0x80)
+  {
+     return theTIA->peek(address);   
+  }
+    debug[0]++;
+  return 0x00;
+}
+
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+void Cartridge3EPlus::poke(uInt16 address, uInt8 value)
+{
+  // If we are writing down into the TIA area...
+  if (address < 0x80)
+  {
+      // ----------------------------------------------------------------------------
+      // Switch banks if necessary. 3F is the ROM hotspot and 3E is the RAM hotspot
+      // ----------------------------------------------------------------------------
+      if (address == 0x3F)
+      {
+        uInt8 seg = (value >> 6) & 0x03;
+        segment(seg, value & 0x3F);         // Map ROM bank into desired segment
+      }
+      else if (address == 0x3E)
+      {
+        uInt8 seg = (value >> 6) & 0x03;
+        segment(seg, (value & 0x3F) + 128); // Map RAM bank into desired segment
+      }
+      
+      theTIA->poke(address, value);         // Pass through to "real" TIA
+  }
+}
+
+// ---------------------------------------------------------------------------
+// Map a bank of ROM or RAM into the desired segment (0-3) of the memory map.
+//
+// Segment 0 is 0xF000 and is 1K bytes in lenth (0x400 bytes)
+// Segment 1 is 0xF400 and is 1K bytes in lenth (0x400 bytes)
+// Segment 2 is 0xF800 and is 1K bytes in lenth (0x400 bytes)
+// Segment 3 is 0xFC00 and is 1K bytes in lenth (0x400 bytes)
+//
+// ROM will map into the entire segment where as RAM will be split
+// as 0x200 bytes of memory space for Read Access and 0x200 bytes for write.
+// ---------------------------------------------------------------------------
+void Cartridge3EPlus::segment(uInt8 seg, uInt8 bank)
+{
+  if(bank < 128)  // ROM bank
+  {
+    myCurrentBank[seg] = bank;
+    uInt32 offset = bank * 0x400;
+    uInt16 shift = mySystem->pageShift();
+
+    // Setup the page access methods for the current bank
+    page_access.device = this;
+    page_access.directPokeBase = 0;
+
+    // Map ROM image into the system
+    for(uInt32 address = 0x1000+((uInt32)0x400*seg); address < 0x1400+((uInt32)0x400*seg); address += (1 << shift))
+    {
+        page_access.directPeekBase = &myImage[offset + (address & 0x03FF)];
+        mySystem->setPageAccess(address >> shift, page_access);
+    }
+  }
+  else // RAM bank
+  {
+    myCurrentBank[seg] = bank;
+    bank -= 128;
+
+    uInt32 offset = bank * 0x200;
+    uInt16 shift = mySystem->pageShift();
+
+    // Setup the page access methods for the current bank
+    page_access.device = this;
+    page_access.directPokeBase = 0;
+
+    // Map read-port RAM image into the system
+    for(uInt32 address = 0x1000+((uInt32)0x400*seg); address < 0x1200+((uInt32)0x400*seg); address += (1 << shift))
+    {
+        page_access.directPeekBase = &my3ERam[offset + (address & 0x01FF)];
+        mySystem->setPageAccess(address >> shift, page_access);
+    }
+
+    page_access.directPeekBase = 0;
+
+    // Map write-port RAM image into the system - this is shifted up 0x200 bytes
+    for(uInt32 address = 0x1200+((uInt32)0x400*seg); address < 0x1400+((uInt32)0x400*seg); address += (1 << shift))
+    {
+        page_access.directPokeBase = &my3ERam[offset + (address & 0x01FF)];
+        mySystem->setPageAccess(address >> shift, page_access);
+    }
+  }    
+}
---
+++ arm9/source/emucore/Cart3EPlus.hxx
@@ -0,0 +1,154 @@
+//============================================================================
+//
+//   SSSS    tt          lll  lll
+//  SS  SS   tt           ll   ll
+//  SS     tttttt  eeee   ll   ll   aaaa
+//   SSSS    tt   ee  ee  ll   ll      aa
+//      SS   tt   eeeeee  ll   ll   aaaaa  --  "An Atari 2600 VCS Emulator"
+//  SS  SS   tt   ee      ll   ll  aa  aa
+//   SSSS     ttt  eeeee llll llll  aaaaa
+//
+// Copyright (c) 1995-2022 by Bradford W. Mott, Stephen Anthony
+// and the Stella Team
+//
+// This file has been modified by Dave Bernazzani (wavemotion-dave)
+// for optimized execution on the DS/DSi platform. Please seek the
+// official Stella source distribution which is far cleaner, newer,
+// and better maintained.
+//
+// See the file "License.txt" for information on usage and redistribution of
+// this file, and for a DISCLAIMER OF ALL WARRANTIES.
+//============================================================================
+#ifndef CARTRIDGE3EPLUS_HXX
+#define CARTRIDGE3EPLUS_HXX
+
+class Cartridge3EPlus;
+
+#include "bspf.hxx"
+#include "Cart.hxx"
+
+/**
+ Cartridge class for new tiling engine "Boulder Dash" format games with RAM.
+  Kind of a combination of 3F and 3E, with better switchability.
+  B.Watson's Cart3E was used as a template for building this implementation.
+  The destination bank (0-3) is held in the top bits of the value written to
+  $3E (for RAM switching) or $3F (for ROM switching). The low 6 bits give
+  the actual bank number (0-63) corresponding to 512 byte blocks for RAM and
+  1024 byte blocks for ROM. The maximum size is therefore 32K RAM and 64K ROM.
+  D7D6         indicate the bank number (0-3)
+  D5D4D3D2D1D0 indicate the actual # (0-63) from the image/ram
+  
+  ROM:
+  Note: In descriptions $F000 is equivalent to $1000 -- that is, we only deal
+  with the low 13 bits of addressing. Stella code uses $1000, I'm used to $F000
+  So, mask with top bits clear :) when reading this document.
+  In this scheme, the 4K address space is broken into four 1K ROM/512b RAM segments
+  living at 0x1000, 0x1400, 0x1800, 0x1C00 (or, same thing, 0xF000... etc.),
+  The last 1K ROM ($FC00-$FFFF) segment in the 6502 address space (ie: $1C00-$1FFF)
+  is initialised to point to the FIRST 1K of the ROM image, so the reset vectors
+  must be placed at the end of the first 1K in the ROM image.
+  Note: This is DIFFERENT to 3E which switches in the UPPER bank and this bank is
+  fixed.  This allows variable sized ROM without having to detect size.
+  ROM switching (write of block+bank number to $3F) D7D6 upper 2 bits of bank #
+  indicates the destination segment (0-3, corresponding to $F000, $F400, $F800,
+  $FC00), and lower 6 bits indicate the 1K bank to switch in.  Can handle 64
+  x 1K ROM banks (64K total).
+  
+  D7 D6 D5D4D3D2D1D0
+  0  0   x x x x x x   switch a 1K ROM bank xxxxxx to $F000
+  0  1                 switch a 1K ROM bank xxxxxx to $F400
+  1  0                 switch a 1K ROM bank xxxxxx to $F800
+  1  1                 switch a 1K ROM bank xxxxxx to $FC00
+  
+  RAM switching (write of segment+bank number to $3E) with D7D6 upper 2 bits of
+  bank # indicates the destination RAM segment (0-3, corresponding to $F000,
+  $F400, $F800, $FC00).
+  Can handle 64 x 512 byte RAM banks (32K total)
+  
+  D7 D6 D5D4D3D2D1D0
+  0  0   x x x x x x   switch a 512 byte RAM bank xxxxxx to $F000 with write @ $F200
+  0  1                 switch a 512 byte RAM bank xxxxxx to $F400 with write @ $F600
+  1  0                 switch a 512 byte RAM bank xxxxxx to $F800 with write @ $FA00
+  1  1                 switch a 512 byte RAM bank xxxxxx to $FC00 with write @ $FE00
+  @author  Thomas Jentzsch and Stephen Anthony
+*/
+
+class Cartridge3EPlus : public Cartridge
+{
+  public:
+    /**
+      Create a new cartridge using the specified image and size
+
+      @param image Pointer to the ROM image
+      @param size The size of the ROM image
+    */
+    Cartridge3EPlus(const uInt8* image, uInt32 size);
+ 
+    /**
+      Destructor
+    */
+    virtual ~Cartridge3EPlus();
+
+  public:
+    /**
+      Get a null terminated string which is the device's name (i.e. "M6532")
+
+      @return The name of the device
+    */
+    virtual const char* name() const;
+
+    /**
+      Reset device to its power-on state
+    */
+    virtual void reset();
+
+    /**
+      Install cartridge in the specified system.  Invoked by the system
+      when the cartridge is attached to it.
+
+      @param system The system the device should install itself in
+    */
+    virtual void install(System& system);
+
+  public:
+    /**
+      Get the byte at the specified address
+
+      @return The byte at the specified address
+    */
+    virtual uInt8 peek(uInt16 address);
+
+    /**
+      Change the byte at the specified address to the given value
+
+      @param address The address where the value should be stored
+      @param value The value to be stored at the address
+    */
+    virtual void poke(uInt16 address, uInt8 value);
+
+  private:
+    /** 
+      Map the specified bank into the first segment
+
+      @param bank The bank that should be mapped
+    */
+    void segment(uInt8 seg, uInt8 bank);
+
+  private:
+    // Indicates which bank is currently active for the segments
+    uInt16 myCurrentBank[4];
+
+    // RAM contents. For now every ROM gets all 32K of potential RAM
+    uInt8 my3ERam[32768];
+    
+    // ROM contents ... up to 64K
+    uInt8 *myImage;
+    
+    uInt8 statingBank;
+    uInt8 myBankMod;
+    
+    // Size of the ROM image
+    uInt32 mySize;
+};
+#endif
+
